31a57ebfbf1d7a04ea490144d0f866192ce9b8b3
GabrielLacorte <gabrielslacorte@hotmail.com>
2025-10-14 22:49:37 -0300

    feat: implementa UsuarioService com operações CRUD e tratamento básico de exceções via RuntimeException

--- src/main/java/com/gabriel/agendador/service/UsuarioService.java
+++ src/main/java/com/gabriel/agendador/service/UsuarioService.java
@@ -1,7 +1,10 @@
 package com.gabriel.agendador.service;
 
+import com.gabriel.agendador.entity.Usuario;
 import com.gabriel.agendador.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -9,4 +12,44 @@ public class UsuarioService {
     public UsuarioService(UsuarioRepository usuarioRepository){
         this.usuarioRepository = usuarioRepository;
     }
+    //GET
+
+    public List<Usuario> listarTodos(){
+        return usuarioRepository.findAll();
+    }
+
+    //GET ID
+    public Usuario buscarPorId(Long id){
+        return usuarioRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Usuario não encontrado"));
+
+    }
+
+    //Post
+    public Usuario salvarUsuario(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    //PUT
+    public Usuario atualizarUsuario (Long id, Usuario usuarioAtualizado){
+        return usuarioRepository.findById(id)
+                .map(usuario ->{
+                    usuario.setEmail(usuarioAtualizado.getEmail());
+                    usuario.setNome(usuarioAtualizado.getNome());
+                    usuario.setSenha(usuarioAtualizado.getSenha());
+                    return usuarioRepository.save(usuario);
+                })
+                .orElseThrow(()->new RuntimeException("Usuario não encontrado com id"));
+    }
+    //Delete
+    public void deletarUsuario(Long id){
+        if(usuarioRepository.existsById(id)){
+
+            usuarioRepository.deleteById(id);
+        }else{
+            throw new RuntimeException("Usuario não encontrado com id" + id);
+        }
+    }
+
 }
+
